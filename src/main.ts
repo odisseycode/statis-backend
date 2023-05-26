2c96b56be1b3bb44603a72a26e3caad98b96ccc0
rdias66 <dias.a@live.com>
2023-05-26 13:01:59 -0300

    imports made on main for future testing

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {meanFunction, medianFunction, standardDeviationFunction, varianceFunction, minFunction, maxFunction} from 'src/functions/descreptive-stats.module'
+import {pdfFunction, cdfFunction, invFunction, sampleFunction} from 'src/functions/probability-stats.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
